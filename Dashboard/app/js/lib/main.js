07dc3b72013a6348bfdd5dad1c5ecff9041a6cce
Iván Perdomo <ivan@akvo.org>
2013-05-29 12:09:10 +0200

    Upgrade to Ember 1.0.0 RC4 and Ember Data 0.13

--- Dashboard/app/js/lib/main.js
+++ Dashboard/app/js/lib/main.js
@@ -3,9 +3,9 @@ require('akvo-flow/vendor/jquery-ui-1.8.21.custom');
 require('akvo-flow/vendor/modernizr.custom.32662');
 require('akvo-flow/vendor/d3.v2.min');
 require('akvo-flow/vendor/jquery.dataTables');
-require('akvo-flow/vendor/handlebars-1.0.0-rc.3');
-require('akvo-flow/vendor/ember-1.0.0-rc.1');
-require('akvo-flow/vendor/ember-data-rev11');
+require('akvo-flow/vendor/handlebars-1.0.0-rc.4');
+require('akvo-flow/vendor/ember-1.0.0-rc.4-3-gbd1629e');
+require('akvo-flow/vendor/ember-data-0.13.js');
 
 require('akvo-flow/all_locales');
 require('akvo-flow/models/FLOWrest-adapter-v2');
