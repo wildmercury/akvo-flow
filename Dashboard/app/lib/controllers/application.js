32e21f953f4b284828d93721f8911e0d319be979
Iván Perdomo <ivan@akvo.org>
2013-08-22 20:00:06 +0200

    Implements basic loading message, plus the survey-detail component

--- Dashboard/app/lib/controllers/application.js
+++ Dashboard/app/lib/controllers/application.js
@@ -1,13 +1,19 @@
 require('akvo-flow/core');
 
 FLOW.ApplicationController = Ember.Controller.extend({
+    _ldng: false,
     init: function () {
-        this._super(arguments);
+        this._super.apply(this, arguments);
         this.get('store').get('adapter').on('ajax', this, 'status');
+        this.get('store').get('adapter').on('didFindAll', this, 'status');
+        this.get('store').get('adapter').on('didFindQuery', this, 'status');
     },
     status: function (type) {
-        console.log('>>> ajax', type);
-    }
+        this.set('_ldng', type === 'GET');
+    },
+    loading: function () {
+        return this.get('_ldng');
+    }.property('_ldng'),
 });
 
 
