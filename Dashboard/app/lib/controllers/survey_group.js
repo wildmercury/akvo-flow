3b533206839c2ec4c38dc32d9e71546b8ccd2e66
Iván Perdomo <ivan@akvo.org>
2013-08-30 11:43:24 +0200

    Redefine actions defined in the controller
    From the Ember RC8 blog post
    
    > we've standardized around using a hash called `actions`.When
    > extending a class with actions defined, we'll merge the actions
    > defined on the subclass or instance with those on the parent.
    > We also support `_super` so you won't lose any flexibility
    > with this approach.
    > (...)
    > we internally rename the property to `_actions` to avoid any
    > conflicts.
    
    http://emberjs.com/blog/2013/08/29/ember-1-0-rc8.html

--- Dashboard/app/lib/controllers/survey_group.js
+++ Dashboard/app/lib/controllers/survey_group.js
@@ -1,40 +1,43 @@
 FLOW.SurveyGroupController = Ember.ObjectController.extend({
     isEditing: false,
 
-    edit: function () {
-        this.set('isEditing', true);
-    },
-
-    cancelEdit: function () {
-        this.set('isEditing', false);
-    },
-
-    saveEdit: function () {
-        var code = this.get('code') && this.get('code').capitalize();
-
-        if (!code) {
-            return;
-        }
-
-        this.set('name', code);
-        this.set('code', code);
-
-        this.get('store').commit();
-
-        this.cancelEdit();
-    },
-
     disableDelete: function () {
         return this.get('surveyCount') !== 0;
     }.property('surveyCount'),
 
-    deleteSurveyGroup: function () {
-        var sg = this.get('model');
+    actions: {
+        edit: function () {
+            this.set('isEditing', true);
+        },
 
-        sg.deleteRecord();
+        cancelEdit: function () {
+            this.get('model').rollback();
+            this.set('isEditing', false);
+        },
 
-        this.get('store').commit();
+        saveEdit: function () {
+            var code = this.get('code') && this.get('code').capitalize();
 
-        this.transitionToRoute('survey_groups');
+            if (!code) {
+                return;
+            }
+
+            this.set('name', code);
+            this.set('code', code);
+
+            this.get('store').commit();
+
+            this._actions.cancelEdit();
+        },
+
+        deleteSurveyGroup: function () {
+            var sg = this.get('model');
+
+            sg.deleteRecord();
+
+            this.get('store').commit();
+
+            this.transitionToRoute('survey_groups');
+        }
     }
 });
--- Dashboard/app/lib/controllers/survey_groups.js
+++ Dashboard/app/lib/controllers/survey_groups.js
@@ -4,29 +4,31 @@ FLOW.SurveyGroupsController = Ember.ArrayController.extend({
 
     creatingNew: false,
 
-    addNewGroup: function () {
-        this.set('creatingNew', true);
-    },
+    actions: {
+        addNewGroup: function () {
+            this.set('creatingNew', true);
+        },
 
-    cancelNewGroup: function () {
-        this.set('newSurveyGroup', '');
-        this.set('creatingNew', false);
-    },
+        cancelNewGroup: function () {
+            this.set('newSurveyGroup', '');
+            this.set('creatingNew', false);
+        },
 
-    createSurveyGroup: function () {
-        var groupName = this.get('newSurveyGroup') && this.get('newSurveyGroup').capitalize();
+        createSurveyGroup: function () {
+            var groupName = this.get('newSurveyGroup') && this.get('newSurveyGroup').capitalize();
 
-        if (!groupName) {
-            return;
+            if (!groupName) {
+                return;
+            }
+
+            FLOW.SurveyGroup.createRecord({
+                code: groupName,
+                name: groupName
+            });
+
+            this.get('store').commit();
+
+            this._actions.cancelNewGroup();
         }
-
-        FLOW.SurveyGroup.createRecord({
-            code: groupName,
-            name: groupName
-        });
-
-        this.get('store').commit();
-
-        this.cancelNewGroup();
     }
 });
