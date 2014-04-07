6a91421e3fbb69a397c36be07f00ba0258ddbc19
Mark Tiele Westra <m.t.westra@akvo.org>
2014-04-07 16:58:01 +0200

    [#493] Create fixtures file

--- Dashboard-v2/src-cljs/akvo_flow/components/surveys.cljs
+++ Dashboard-v2/src-cljs/akvo_flow/components/surveys.cljs
@@ -4,7 +4,8 @@
     [sablono.core :as html :refer-macros [html]]
     [om.core :as om :include-macros true]
     [om.dom :as dom :include-macros true]
-    [akvo-flow.dispatch :refer [body]]))
+    [akvo-flow.dispatch :refer [body]]
+    [akvo-flow.fixtures.fixtures :refer [survey-groups]]))
 
 (defn surveys-tab-group-sidebar [state owner]
   (reify
@@ -46,20 +47,3 @@
              (om/build surveys-tab-group-topbar state)
              ]))))
 
-;; fixtures
-(def survey-groups 
-  [{:key 1 :display-name "group 1" :id 121221}
-   {:key 2 :display-name "group 2" :id 123213}
-   {:key 3 :display-name "group 3" :id 443343}
-   {:key 4 :display-name "group 4" :id 554343}
-   {:key 5 :display-name "group 5" :id 553232}
-   {:key 6 :display-name "group 6" :id 234323}
-   {:key 7 :display-name "group 7" :id 667766}
-   {:key 8 :display-name "group 8" :id 123455}
-   {:key 9 :display-name "group 9" :id 323234}
-   {:key 10 :display-name "group 10" :id 545455}
-   {:key 11 :display-name "group 11" :id 11111}
-   {:key 12 :display-name "group 12" :id 22222}
-   {:key 13 :display-name "group 13" :id 33333}
-   {:key 14 :display-name "group 14" :id 44444}
-   {:key 15 :display-name "group 15" :id 55555}])
---
+++ Dashboard-v2/src-cljs/akvo_flow/fixtures/fixtures.cljs
@@ -0,0 +1,18 @@
+(ns akvo-flow.fixtures.fixtures)
+ 
+(def survey-groups 
+  [{:key 1 :display-name "group 1" :id 121221}
+   {:key 2 :display-name "group 2" :id 123213}
+   {:key 3 :display-name "group 3" :id 443343}
+   {:key 4 :display-name "group 4" :id 554343}
+   {:key 5 :display-name "group 5" :id 553232}
+   {:key 6 :display-name "group 6" :id 234323}
+   {:key 7 :display-name "group 7" :id 667766}
+   {:key 8 :display-name "group 8" :id 123455}
+   {:key 9 :display-name "group 9" :id 323234}
+   {:key 10 :display-name "group 10" :id 545455}
+   {:key 11 :display-name "group 11" :id 11111}
+   {:key 12 :display-name "group 12" :id 22222}
+   {:key 13 :display-name "group 13" :id 33333}
+   {:key 14 :display-name "group 14" :id 44444}
+   {:key 15 :display-name "group 15" :id 55555}])
