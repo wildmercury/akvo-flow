69acc8400b3a26c3c294896664ea93c0ccca33b5
Jonas Enlund <jonas.enlund@gmail.com>
2014-08-26 13:09:34 +0300

    [#493] Use a default map for ajax requests

--- Dashboard-v3/src/org/akvo/flow/app_state.cljs
+++ Dashboard-v3/src/org/akvo/flow/app_state.cljs
@@ -1,44 +1,43 @@
 (ns org.akvo.flow.app-state
   (:require [org.akvo.flow.dispatcher :as dispatcher]
             [cljs.core.async :as async]
-            [ajax.core :refer (ajax-request json-format POST PUT DELETE)])
+            [ajax.core :refer (ajax-request json-format GET POST PUT DELETE)])
   (:require-macros [cljs.core.async.macros :refer (go go-loop)]))
 
 (def app-state (atom {:current-page {:path [:surveys]}}))
 
-(ajax-request "/rest/devices" :get
-              {:handler (fn [[ok response]]
-                          (if ok
-                            (swap! app-state
-                                   assoc
-                                   :devices
-                                   (get response "devices"))
-                            (.error js/console (str response))))
-               :format (json-format {:keywords? false})})
+(def default-ajax-config
+  {:error-handler #(.error js/console %)
+   :format (json-format {:keywords? false})
+   :response-format :json
+   :keywords? false})
 
-(ajax-request "/rest/users" :get
-              {:handler (fn [[ok response]]
-                          (if ok
-                            (swap! app-state
-                                   assoc
-                                   :users
-                                   (get response "users"))
-                            (.error js/console (str response))))
-               :format (json-format {:keywords? false})})
+(GET "/rest/devices"
+     (merge default-ajax-config
+            {:handler (fn [response]
+                        (swap! app-state
+                               assoc
+                               :devices
+                               (get response "devices")))}))
+
+(GET "/rest/users"
+     (merge default-ajax-config
+            {:handler (fn [{:strs [users]}]
+                        (swap! app-state
+                               assoc
+                               :users
+                               users))}))
 
 
 (let [chan (dispatcher/register :new-user)]
   (go-loop []
     (let [[_ new-user] (<! chan)]
       (POST "/rest/users"
-            {:params {"user" new-user}
-             :handler (fn [response]
-                        (let [user (get response "user")]
-                          (swap! app-state update-in [:users] conj user)))
-             :error-handler #(.error js/console %)
-             :format (json-format {:keywords? false})
-             :response-format :json
-             :keywords? false}))
+            (merge default-ajax-config
+                   {:params {"user" new-user}
+                    :handler (fn [response]
+                               (let [user (get response "user")]
+                                 (swap! app-state update-in [:users] conj user)))})))
     (recur)))
 
 (defn index-of
@@ -55,17 +54,13 @@
     (let [[_ {:keys [new-value old-value]}] (<! chan)
           idx (index-of old-value (:users @app-state))]
       (PUT (str "/rest/users/" (get new-value "keyId"))
-           {:params {"user" new-value}
-            :handler (fn [response]
-                       (if (>= idx 0)
-                         (swap! app-state assoc-in [:users idx] new-value)
-                         (println "No such user " old-value)))
-            :error-handler #(.error js/console %)
-            :format (json-format {:keywords? false})
-            :response-format :json
-            :keywords? :false}))
+           (merge default-ajax-config
+                  {:params {"user" new-value}
+                   :handler (fn [response]
+                              (if (>= idx 0)
+                                (swap! app-state assoc-in [:users idx] new-value)
+                                (println "No such user " old-value)))})))
     (recur)))
-
 
 (defn remove-idx
   "Remove item at index i from vector v"
@@ -78,14 +73,11 @@
     (let [[_ user] (<! chan)
           idx (index-of user (:users @app-state))]
       (DELETE (str "/rest/users/" (get user "keyId"))
-              {:handler (fn [response]
-                          (if (>= idx 0)
-                            (swap! app-state update-in [:users] remove-idx idx)
-                            (println "No such user " user)))
-               :error-handler #(.error js/console %)
-               :format (json-format {:keywords? false})
-               :response-format :json
-               :keywords? false}))
+              (merge default-ajax-config
+                     {:handler (fn [response]
+                                 (if (>= idx 0)
+                                   (swap! app-state update-in [:users] remove-idx idx)
+                                   (println "No such user " user)))})))
     (recur)))
 
 (let [chan (dispatcher/register :new-access-key)]
