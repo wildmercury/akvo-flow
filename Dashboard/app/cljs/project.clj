dc770758ceea9b0ec3de5c2b6f27f184bd257bf5
Jonas Enlund <jonas.enlund@gmail.com>
2014-10-13 12:29:12 +0300

    [#493] Update cljs dependencies

--- Dashboard/app/cljs/project.clj
+++ Dashboard/app/cljs/project.clj
@@ -3,10 +3,10 @@
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.7.0-alpha2"]
-                 [org.clojure/clojurescript "0.0-2356"]
+                 [org.clojure/clojurescript "0.0-2371"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [om "0.7.3"]
-                 [cljs-ajax "0.3.2"]
+                 [cljs-ajax "0.3.3"]
                  [sablono "0.2.22"]]
 
   :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]]
